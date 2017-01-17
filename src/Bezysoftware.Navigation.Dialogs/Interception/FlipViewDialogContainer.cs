369b03938b995cd49ed5f090dacdc5fcb9187a2b
bezysoftware <bezysoftware@hotmail.cz>
2017-01-17 15:47:09 +0100

    FlipViewDialogContainer

---
+++ src/Bezysoftware.Navigation.Dialogs/Interception/FlipViewDialogContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace Bezysoftware.Navigation.Dialogs.Interception
+{
+    using Bezysoftware.Navigation.Dialogs.View;
+
+    public class FlipViewDialogContainerAttribute : DialogContainerAttribute
+    {
+        public readonly string FlipViewName;
+
+        public FlipViewDialogContainerAttribute(string flipViewName) : base(typeof(FlipViewDialogContainer))
+        {
+            this.FlipViewName = flipViewName;
+        }
+    }
+}
---
+++ src/Bezysoftware.Navigation.Dialogs/View/FlipViewDialogContainer.cs
@@ -0,0 +1,42 @@
+﻿namespace Bezysoftware.Navigation.Dialogs.View
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Bezysoftware.Navigation.Dialogs.Interception;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public class FlipViewDialogContainer : IDialogContainer
+    {
+        private Stack<FlipViewItem> activatedItems = new Stack<FlipViewItem>();
+        private FlipView flipView;
+
+        public void Hide(Type viewType)
+        {
+            this.activatedItems.Pop();
+            var top = this.activatedItems.Peek();
+            this.flipView.SelectedItem = top;
+        }
+
+        public void Show(Type viewType)
+        {
+            var attr = viewType.GetTypeInfo().GetCustomAttribute<FlipViewDialogContainerAttribute>();
+
+            this.flipView = Window.Current.Content.FindVisualChildren<FlipView>().FirstOrDefault(cp => cp.Name == attr.FlipViewName);
+            var flipViewItem = flipView.Items.Cast<FlipViewItem>().First(item => item.Content.GetType() == viewType);
+
+            if (this.activatedItems.Count == 0)
+            {
+                this.activatedItems.Push((FlipViewItem)flipView.Items[flipView.SelectedIndex]);
+            }
+
+            this.activatedItems.Push(flipViewItem);
+
+            flipView.SelectedItem = flipViewItem;
+        }
+    }
+}
