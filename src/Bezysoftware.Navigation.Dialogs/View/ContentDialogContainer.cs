a248fd7166bf6420d6ca7936973f7af1b95fe306
bezysoftware <bezysoftware@hotmail.cz>
2017-04-23 14:05:27 +0200

    Added ContentDialogContainer

---
+++ src/Bezysoftware.Navigation.Dialogs/View/ContentDialogContainer.cs
@@ -0,0 +1,26 @@
+﻿namespace Bezysoftware.Navigation.Dialogs.View
+{
+    using System;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public class ContentDialogContainer : IDialogContainer
+    {
+        private ContentDialog dialog;
+        
+        public async void Show(Type viewType)
+        {
+            var child = (UIElement)Activator.CreateInstance(viewType);
+            this.dialog = new ContentDialog() { Content = child };
+
+            await dialog.ShowAsync();
+        }
+
+        public void Hide(Type viewType)
+        {
+            this.dialog?.Hide();
+            this.dialog = null;
+        }
+    }
+}
